b75b2ed321b0e762af912e1518ccdd231acb3ae0
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-06-17 16:58:40 +0200

    Create cmdhandler.js

---
+++ cmdhandler.js
@@ -0,0 +1,29 @@
+// Load the commands
+const bitcoin = require('./commands/bitcoin.js');
+const luck = require('./commands/luck.js');
+
+// Define command symbol
+const prefix = ".";
+
+// Keep all commands in one place
+const cmds = { bitcoin, luck }
+
+// Command handler function
+module.exports = async function (msg) {
+  
+    // Just to be safe, ignore own messages
+    if (msg.author.bot) return;
+  
+    // If message is a command and starts with our prefix
+    if (msg.content.startsWith(prefix)) {
+      
+        // Split message into command name and its arguments if has any
+        const [cmd, ...args] = msg.content
+            .trim()
+            .substring(prefix.length)
+            .split(/\s+/);
+      
+        // Determine which command was called with const cmds defined above
+        cmds[cmd](msg, args);
+    }
+};
