cdeb536dd8e00e87df6cdea82e86a6f11bdcf894
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-06-17 20:38:57 +0200

    Update ethereum.js

--- commands/ethereum.js
+++ commands/ethereum.js
@@ -1,6 +1,10 @@
+// Import request-promise used to get data from an api
 const rp = require('request-promise');
 
+// Ethereum function
 module.exports = function (msg, args) {
+  
+    // Specify request options, api url, and more
     const requestOptions = {
         method: 'GET',
         uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
@@ -15,15 +19,21 @@ module.exports = function (msg, args) {
         json: true,
         gzip: true
     };
-
+    
+    // Perform request to our api and collect the data we need
     rp(requestOptions).then(response => {
         const price = response['data'][0]['quote']['USD']['price'];
+      
+        // Convert bitcoin value to USD dollars, using the current bitcoin price that we just recieved
         const converted = parseFloat(args[0]) * price;
         const converted2 = converted.toString().split('.');
         const realNumber = converted2[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         const finalNumber = realNumber + '.' + converted2[1];
-
+        
+        // Reply to us on discord with the response
         msg.reply(`${args} ethereum is currently worth $${finalNumber}`);
+      
+    // Catch errors if any occur
     }).catch((err) => {
         console.log('API call error:', err.message);
     });
