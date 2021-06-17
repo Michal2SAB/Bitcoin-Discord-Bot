d2ecdef5edaf8e04710de47bd6bf5d1712cfc2f7
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-06-17 16:30:21 +0200

    Update run.js - Better way of handling commands
    Changed the way bot handles commands, using separate files.

--- run.js
+++ run.js
@@ -1,119 +1,19 @@
+// Import discord.js and define client
 const { Client } = require('discord.js');
-const rp = require('request-promise');
-const CoinKey = require('coinkey');
-const fetch2 = require('node-fetch');
-
 const client = new Client();
-const prefix = "/"
 
+// Import command handler
+const CMDHandler = require('./cmdhandler');
+
+// Set user status and let us know the bot started
 client.on('ready', () => {
     client.user.setActivity("/luck", {type: 3})
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}`);
+    console.log("");
 });
 
-client.on('message', (msg) => {
-    if (msg.author.bot) return;
-    if (msg.content.startsWith(prefix)) {
-        const [cmd, ...args] = msg.content
-            .trim()
-            .substring(prefix.length)
-            .split(/\s+/);
+// Handle commands
+client.on('message', CMDHandler);
 
-        if (cmd === "bitcoin") {
-            bitcoin(msg, args);
-        }
-
-        if (cmd === "luck") {
-            luck(msg);
-        }
-    }
-});
-
-function bitcoin(msg, args) {
-    const requestOptions = {
-        method: 'GET',
-        uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
-        qs: {
-            'start': '1',
-            'limit': '1',
-            'convert': 'USD'
-        },
-        headers: {
-            'X-CMC_PRO_API_KEY': process.env.api_key
-        },
-        json: true,
-        gzip: true
-    };
-
-    rp(requestOptions).then(response => {
-        const price = response['data'][0]['quote']['USD']['price'];
-        const converted = parseFloat(args[0]) * price;
-        const converted2 = converted.toString().split('.');
-        const realNumber = converted2[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        const finalNumber = realNumber + '.' + converted2[1];
-
-        msg.reply(`${args} bitcoin is currently worth $${finalNumber}`);
-    }).catch((err) => {
-        console.log('API call error:', err.message);
-    });
-};
-
-var i = 0;
-const wallets = {};
-
-async function luck(msg) {
-    process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-    const settings = { method: "Get" };
-    const url = 'https://blockchain.info/balance?active='
-    setTimeout(async function() { 
-        const ck = new CoinKey(Buffer.from(r(64), 'hex'));
-        ck.compressed = false;
-
-        const pub = ck.publicAddress;
-        const priv = ck.privateWif;
-
-        await fetch2(url + pub.toString(), settings)
-        .then(res => res.json())
-        .then(json => {
-            const balance = json[pub]['final_balance'];
-            wallets[pub] = priv + "," + balance;
-        })
-        .catch(error => {
-            console.log("error");
-        });
-        i++;
-        if (i < 10) {
-            luck(msg);
-        } else {
-            var finalWallets = "";
-            for (const [key, value] of Object.entries(wallets)) {
-                const vSeparated = value.split(",");
-                finalWallets = finalWallets + `[${vSeparated[0]}](https://www.blockchain.com/btc/address/${key}) ($${vSeparated[1]})\n\n`;
-            }
-            const Discord = require('discord.js');
-            const embed = new Discord.MessageEmbed()
-            .setColor('#f7941a')
-            .setTitle('Random Seeds')
-            .setAuthor('Bitcoin', 'https://bitmarket.pl/wp-content/uploads/2020/10/Bitcoin.svg_.png')
-            .setDescription(finalWallets);
-
-            msg.reply(embed);
-            i = 0;
-            for (const [key, value] of Object.entries(wallets)) {
-                delete wallets[key];
-            }
-        }
-    }, 200)
-};
-
-function r(l) {
-    let randomChars = 'ABCDF0123456789';
-    let result = '';
-    for ( var i = 0; i < l; i++ ) {
-        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
-    }
-    return result;
-};
-
+// Login to discord with bot token
 client.login(process.env.bot_token);
-
